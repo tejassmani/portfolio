0d477c88f3813a7ab825c48448cfdceb677fcefc
tejassmani <tmani@ucsd.edu>
2025-05-30 10:51:52 -0700

    stable circles

--- meta/meta.js
+++ meta/meta.js
@@ -59,7 +59,7 @@ function updateScatterPlot(data, commits) {
   const sortedCommits = d3.sort(commits, (d) => -d.totalLines);
   dots
     .selectAll("circle")
-    .data(sortedCommits)
+    .data(sortedCommits, (d) => d.id)
     .join("circle")
     .attr("cx", (d) => xScale(d.datetime))
     .attr("cy", (d) => yScale(d.hourFrac))
@@ -254,7 +254,7 @@ function renderScatterPlot(data, allCommits) {
     .range([2, 30]);
   dots
     .selectAll("circle")
-    .data(sortedCommits)
+    .data(sortedCommits, (d) => d.id)
     .join("circle")
     .attr("cx", (d) => xScale(d.datetime))
     .attr("cy", (d) => yScale(d.hourFrac))
